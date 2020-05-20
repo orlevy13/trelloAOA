973df6b79f69ecd793c3d6110fe721a49e8d214e
Alon Yaari <shmely@gmail.com>
2020-05-20 20:50:27 +0300

    menu and nav partly

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { PhaseList } from '../cmps/PhaseList';
 import { boardService } from '../services/boardService';
-import Icon from '@material-ui/core/Icon';
+import { SearchOutlined, LabelOutlined, ListOutlined } from '@material-ui/icons';
+
 import { MemberInitials } from '../cmps/MemberInitials.jsx';
 
 
 class _Board extends Component {
 
     state = {
+        showMenu: false,
         board: null
     }
 
@@ -23,8 +25,15 @@ class _Board extends Component {
 
 
     }
+    toggleMenu = () => {
+        this.setState({ showMenu: !this.state.showMenu });
+
+
+
+    }
     render() {
         const { board } = this.state;
+        const menuClass = !this.state.showMenu ? 'board-menu display-none' : 'board-menu'
 
 
         return (
@@ -38,9 +47,41 @@ class _Board extends Component {
                         <div className="board-members">
                             {board.members.map((member) => <MemberInitials member={member} />)}
                         </div>
-                        <span className="nav-invite-btn">Invite</span>
+                        <span className="nav-btn">Invite</span>
                     </div>
-                    <span className="nav-invite-btn">...Show Menu</span>
+                    <span className="nav-btn" onClick={this.toggleMenu} >...Show Menu</span>
+                    <div className={menuClass}>
+                        <div className="flex column">
+                            <div className="board-menu-header flex justify-center">
+                                <h3 className="menu-header-title">Menu</h3>
+                                <span onClick={this.toggleMenu} className="btn-close-board-menu">X</span>
+                            </div>
+                            <span className="board-menu-header-divider"></span>
+                            <div className="board-menu-content">
+                                <div className="board-menu-item flex align-center">
+                                    <div className="board-menu-icon" style={{ "background-color": board.bgColor }}></div>
+                                    <span className="board-menu-text">Change Background</span>
+                                </div>
+                                <div className="board-menu-item flex align-center">
+                                    <SearchOutlined className="board-menu-icon" />
+                                    <span className="board-menu-text">Search Cards</span>
+                                </div>
+                                <div className="board-menu-item flex align-center">
+                                    <LabelOutlined className="board-menu-icon" />
+                                    <span className="board-menu-text">Stickers</span>
+                                </div>
+                                <span className="board-menu-header-divider"></span>
+                                <div className="board-menu-item flex align-center">
+                                    <ListOutlined className="board-menu-icon" />
+                                    <span className="board-menu-text">Activity</span>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+
+                    </div>
                 </section>
                 <section className="board-content flex">
                     <PhaseList phases={[examplePhase]} />
