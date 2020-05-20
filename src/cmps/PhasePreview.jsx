e4cd4e0a0d03fdaad621b76b2a4841434c84a1e5
orlevy13 <orlevy13d@gmail.com>
2020-05-20 16:45:56 +0300

    third commit

--- src/cmps/PhasePreview.jsx
+++ src/cmps/PhasePreview.jsx
@@ -1,11 +1,57 @@
 import React, { Component } from 'react';
 
 export class PhasePreview extends Component {
-    render() {
-        return (
-            <div>
+    state = {
+        isInputShown: false,
+        newPhaseName: ''
+    }
 
-            </div>
+    componentDidMount() {
+        this.setState({ newPhaseName: this.props.phase.name })
+    }
+    componentWillUnmount() {
+        window.removeEventListener('keydown', this.hideInput);
+    }
+
+
+    toggleInputShown = () => {
+        if (!this.state.isInputShown) window.addEventListener('keydown', this.hideInput);
+        else window.removeEventListener('keydown', this.hideInput);
+        this.setState(prevState => ({ isInputShown: !prevState.isInputShown }))
+    }
+
+    hideInput = (ev) => {
+        if (ev.code === 'Escape') {
+            this.setState({ isInputShown: false });
+            window.removeEventListener('keydown', this.hideInput);
+        }
+    }
+
+    handleChange = ({ target }) => {
+        this.setState({ newPhaseName: target.value })
+    }
+
+    handleSubmit = (ev) => {
+        ev.preventDefault();
+        console.log('Changing phase name:', this.state.newPhaseName)
+        this.toggleInputShown();
+    }
+
+    render() {
+        const { name } = this.props.phase;
+        const { newPhaseName, isInputShown } = this.state;
+        return (
+            <article className="phase">
+                <div className="phase-header flex space-between">
+                    {!isInputShown && <h5 className="phase-title"
+                        onClick={this.toggleInputShown}>{name}</h5>}
+                    {isInputShown && <form className="flex grow" onSubmit={this.handleSubmit}>
+                        <input className="grow" type="text" name="newPhaseName" value={newPhaseName}
+                            autoFocus autoComplete="off" onChange={this.handleChange} />
+                    </form>}
+                    <button>•••</button>
+                </div>
+            </article>
         );
     }
 }
--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -10,7 +10,7 @@ class _Board extends Component {
 
                 </section>
                 <section className="board-content flex">
-
+                    <PhaseList phases={[examplePhase]} />
                 </section>
             </main>
         )
@@ -26,3 +26,68 @@ const mapDispatchToProps = {
 }
 
 export const Board = connect(mapStateToProps, mapDispatchToProps)(_Board)
+
+const examplePhase = {
+    id: 'dsbnjinijfndb',
+    name: 'ideas',
+    desc: 'stam ideas',
+    cards:
+        [
+            {
+                id: 'vfdbfb fds bnfsda njbas',   // our id                         
+                bgColor: '#ddd',
+                title: 'create-react-app ready to start',
+                desc: 'using cli create new project',
+                dueDate: 159221158158,
+                createdAt: 159221158158, //optional
+                labels:
+                    [
+                        {
+                            id: 'dvsdvdsv',
+                            color: '#fff',
+                            txt: 'important'
+                        },
+                        {
+                            id: 'gdfgfsd',
+                            color: '#aaa',
+                            txt: 'good'
+                        }
+                    ],
+                checkList:
+                    [
+                        {
+                            id: 'bakdsa',
+                            txt: 'do your homework',
+                            isDone: true
+
+                        },
+                        {
+                            id: 'bakdsjk',
+                            txt: 'do your homework',
+                            isDone: false
+                        }
+                    ],
+                assignedTo:
+                    [
+                        {
+                            _id: 'ffdgdfsgbsfdbsfd',
+                            fullName: 'Aviad Guest',
+                            img: 'http://cloudinary.com/aviad',
+                        },
+                        {
+                            _id: 'ffdgdfsgbsfdbsfd',
+                            fullName: 'or Guest',
+                            img: 'http://cloudinary.com/or',
+                        }
+                    ],
+                attachments:
+                    [
+                        {
+                            _id: '????vdfvdf',
+                            name: 'project-1.pdf',
+                            url: 'http://cloudinary.com/vhfdbhvhbfd'
+                        }
+                    ]
+            }
+        ]
+}
